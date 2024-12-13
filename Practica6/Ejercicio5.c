e6cad5dca37a2819479146213402255b5876085c
adrianql5 <adrianquiroga5@gmail.com>
2024-12-13 13:17:28 +0100

    duro

--- Practica6/Ejercicio5.c
+++ Practica6/Ejercicio5.c
@@ -9,151 +9,134 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-void manejador(int sig){
-    printf("A");
+void manejador(int sig) {
+    // Maneja señales pero no realiza ninguna acción específica
 }
 
 pid_t hijo;
 
-
-int calcularLongSalida(char * mapaEntrada, int longitudEntrada){
+int calcularLongSalida(char *mapaEntrada, int longitudEntrada) {
     int longitudSalida = longitudEntrada;
-    for(int i = 0; i < longitudEntrada; i++){
-        if(mapaEntrada[i] <= '9' && mapaEntrada[i] >= '0'){
-            longitudSalida += mapaEntrada[i] - '0';
+    for (int i = 0; i < longitudEntrada; i++) {
+        if (mapaEntrada[i] >= '0' && mapaEntrada[i] <= '9') {
+            longitudSalida += mapaEntrada[i] - '1';
         }
     }
     return longitudSalida;
 }
 
-
-void procesarMayusculasYEspacios(char *mapaEntrada, char *mapaSalida, int longitudEntrada, int longitudSalida){
+void procesarMayusculasYEspacios(char *mapaEntrada, char *mapaSalida, int longitudEntrada, int longitudSalida) {
     char *buffer = (char *)malloc(longitudSalida);
-    memset(buffer,0,longitudSalida);
+    memset(buffer, 0, longitudSalida);
     int j = 0;
 
-    for(int i=0; i<longitudEntrada; i++){
-        if(i == ((longitudEntrada/2)-1)){
-            memcpy(mapaSalida, buffer, (longitudSalida/2) -1);
+    for (int i = 0; i < longitudEntrada; i++) {
+        if (isupper(mapaEntrada[i])) {
+            buffer[j++] = tolower(mapaEntrada[i]);
+        } else if (isdigit(mapaEntrada[i])) {
+            for (int k = 0; k < mapaEntrada[i] - '0'; k++) {
+                buffer[j++] = '9';
+            }
+        } else {
+            buffer[j++] = mapaEntrada[i];
+        }
+
+        if (i == (longitudEntrada / 2) - 1) {
+            memcpy(mapaSalida, buffer, j);
             kill(hijo, SIGUSR1);
         }
-        if(isupper(mapaEntrada[i])){
-            buffer[j] = tolower(mapaEntrada[i]);
-            j++;
-        }
-        else if(mapaEntrada[i]<=9 && mapaEntrada[i]>=0){
-            for(int k = 0; k < mapaEntrada[i]; k++){
-                buffer[j] = '9';
-                j++;
-            }
-        }
-        else{
-            buffer[i] = mapaEntrada[i];
-        }
     }
-    memcpy(mapaSalida+(longitudSalida/2), buffer+(longitudSalida/2), longitudSalida-(longitudSalida/2));
+
+
+
+    memcpy(mapaSalida , buffer, longitudSalida);
     free(buffer);
     kill(hijo, SIGUSR1);
 }
 
-void procesarNumeros(char *mapaSalida,int i, int longitudSalida){
-    for(int i; i < longitudSalida; i++){
-        if(mapaSalida[i] == '9') mapaSalida[i] = '*';
+void procesarNumeros(char *mapaSalida, int inicio, int fin) {
+    for (int i = inicio; i < fin; i++) {
+        if (mapaSalida[i] == '9') {
+            mapaSalida[i] = '*';
+        }
     }
 }
 
-
-
-
-
-
-int main(int argc, char ** argv){
-
+int main(int argc, char **argv) {
     int descEntrada, descSalida;
-
     struct stat statsEntrada;
-
     int longitudEntrada;
-
     char *mapaEntrada, *mapaSalida;
 
-    if(argc!=3){
+    if (argc != 3) {
         printf("Error en los argumentos\n");
         return -1;
     }
 
-    if ((descEntrada = open(argv[1], O_RDONLY)) < 0)
-    {
+    if ((descEntrada = open(argv[1], O_RDONLY)) < 0) {
         perror("Descriptor de entrada");
         return 1;
     }
 
-    if ((descSalida = open(argv[2], O_RDWR | O_TRUNC | O_CREAT, (mode_t) 0666)) < 0)
-    {
+    if ((descSalida = open(argv[2], O_RDWR | O_TRUNC | O_CREAT, (mode_t)0666)) < 0) {
         perror("Descriptor de salida");
         return 1;
     }
 
-    if (fstat(descEntrada, &statsEntrada) < 0)
-    {
+    if (fstat(descEntrada, &statsEntrada) < 0) {
         perror("Fstat");
         return 1;
     }
 
     longitudEntrada = statsEntrada.st_size;
-
     mapaEntrada = mmap(NULL, longitudEntrada, PROT_READ, MAP_SHARED, descEntrada, 0);
-    if (mapaEntrada == MAP_FAILED)
-    {
+    if (mapaEntrada == MAP_FAILED) {
         perror("mmap Entrada");
         return 1;
     }
 
     int longitudSalida = calcularLongSalida(mapaEntrada, longitudEntrada);
-
     if (ftruncate(descSalida, longitudSalida) < 0) {
         perror("ftruncate");
         return 1;
     }
 
-    mapaSalida = mmap(NULL, longitudSalida, PROT_WRITE|PROT_WRITE, MAP_SHARED, descSalida, 0);
-    if(mapaSalida == MAP_FAILED){
+    mapaSalida = mmap(NULL, longitudSalida, PROT_WRITE, MAP_SHARED, descSalida, 0);
+    if (mapaSalida == MAP_FAILED) {
         perror("mmap Salida");
         return 1;
     }
 
     signal(SIGUSR1, manejador);
-
     hijo = fork();
-    if(hijo == 0){
+
+    if (hijo == 0) { // Proceso hijo
         kill(getppid(), SIGUSR1);
         pause();
-        procesarNumeros(mapaSalida, 0, (longitudSalida/2)-1);
+        procesarNumeros(mapaSalida, 0, longitudSalida / 2);
+        kill(getppid(), SIGUSR1);
         pause();
-        procesarNumeros(mapaSalida, longitudSalida/2, longitudSalida);
+        procesarNumeros(mapaSalida, longitudSalida / 2, longitudSalida);
         exit(0);
-
-    }
-
-    else{
-
+    } else {
+        pause();
         procesarMayusculasYEspacios(mapaEntrada, mapaSalida, longitudEntrada, longitudSalida);
         wait(NULL);
-
     }
 
-    if(munmap(mapaEntrada, longitudEntrada) < 0){
+    if (munmap(mapaEntrada, longitudEntrada) < 0) {
         perror("munmap Entrada");
         return 1;
     }
 
-    if(munmap(mapaSalida, longitudSalida) < 0){
+    if (munmap(mapaSalida, longitudSalida) < 0) {
         perror("munmap Salida");
         return 1;
     }
 
+    close(descEntrada);
+    close(descSalida);
+
     return 0;
-
-
 }
 
